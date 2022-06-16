be086a2a0458de731c382108daf5852a77fcaf3d
Virgil <lunguvirgil3000@gmail.com>
2022-06-16 08:19:50 +0300

    Create calculator function with three arguments,of which one is an operator and the other two are numbers

--- java.js
+++ java.js
@@ -17,3 +17,38 @@ const divide = function(a, b) {
     let quotient = a / b;
     return quotient;
 }
+
+function calculator (a, b, c) {
+  let output = 0;
+
+  try {
+
+    switch(c) {
+      case '+':
+        output = a + b
+        break;
+
+      case '*':
+        output = a * b
+        break;
+
+      case '-':
+        output = a - b
+        break;
+
+     case '/': 
+        if (b === 0) {
+          throw "Can't divide by 0!"
+        } else {
+          output = a / b
+        }
+        break;
+    }
+  }
+  catch(e) {
+    console.log("There's an error: ", e)
+  }
+
+  return output;
+}
+
