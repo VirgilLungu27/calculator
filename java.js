d16d6ce48e95f3a7b9392ebd606dbbb650ab16b1
Virgil <lunguvirgil3000@gmail.com>
2022-06-16 08:11:55 +0300

    Create add, subtract, divide and multiply functions

---
+++ java.js
@@ -0,0 +1,19 @@
+const add = function(a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+const subtract = function(a, b) {
+    let remainder = a - b;
+    return remainder;
+}
+
+const multiply = function(a, b) {
+    let multi = a * b;
+    return multi;
+}
+
+const divide = function(a, b) {
+    let quotient = a / b;
+    return quotient;
+}
